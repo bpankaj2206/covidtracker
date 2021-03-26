3bca73afb2a9fba7cec952be39ba36565eb502f9
bpankaj2206 <bpankaj2206@gmail.com>
2021-03-26 13:13:03 +0530

    Updates

---
+++ precache-manifest.66e46e4d7ee807b9a9dd2e0384080066.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd904c00946385b0b46d70c5cc67400",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "f084a13e3b23952fc6ce",
+    "url": "/covidtracker/static/css/main.5bdb8d4e.chunk.css"
+  },
+  {
+    "revision": "8a53a512730d69466475",
+    "url": "/covidtracker/static/js/2.efb71349.chunk.js"
+  },
+  {
+    "revision": "3df497401d324ee6ed76d4d98715bc5f",
+    "url": "/covidtracker/static/js/2.efb71349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f084a13e3b23952fc6ce",
+    "url": "/covidtracker/static/js/main.56721d98.chunk.js"
+  },
+  {
+    "revision": "43c7d8ceacc7815364f9",
+    "url": "/covidtracker/static/js/runtime-main.b5555a76.js"
+  },
+  {
+    "revision": "39dadbc3044028dbfb93440642b1b220",
+    "url": "/covidtracker/static/media/active.39dadbc3.svg"
+  },
+  {
+    "revision": "7ef8dd6a6ebd88f28a727464dbbb0037",
+    "url": "/covidtracker/static/media/deaths.7ef8dd6a.svg"
+  },
+  {
+    "revision": "c526ae2af35803d90fc228f45086187b",
+    "url": "/covidtracker/static/media/recovered.c526ae2a.svg"
+  },
+  {
+    "revision": "1058b2a5bf04321fa85a97dba3f048e2",
+    "url": "/covidtracker/static/media/total.1058b2a5.svg"
+  }
+]);
